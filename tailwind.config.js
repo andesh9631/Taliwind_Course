b573aea453f5b7ab982e04a66359d0434f7fa1be
Andesh Kumar Sah <sahandesh477@gmail.com>
2024-05-22 17:45:41 +0530

    course

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],     //content: ["./dist/index.html"], (* means all html file read )
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
